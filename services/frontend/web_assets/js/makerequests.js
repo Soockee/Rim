989f6e7055c670126e4f7c1bcfa5f51600ad3405
mario_scrock <mario.scrocca@gmail.com>
2018-05-10 11:51:26 +0200

    Exploit closures to remove varName and arguments of function, refactoring of code structure (moving all variables up), JSLint fixes

--- services/frontend/web_assets/js/makerequests.js
+++ services/frontend/web_assets/js/makerequests.js
@@ -14,51 +14,202 @@ function MakeRequests(opt) {
   //CONFIG
   //buttonName: name of the class of buttons to click
   //putMeHere: class of the div where to append library-generated HTML
-  //varName: name given to the MakeRequests instance
   var buttonName = opt.buttonClassName,
     putMeHere = opt.putMeHereClassName,
-    varName = opt.varName,
     //Functions config
-    beta = true, //false,
-    chisquare = true, //false,
-    exponential = true, //false,
-    normal = true, //false,
-    studentt = true, //false,
-    uniform = true, //false,
-    linear = true, //false,
-    stepFunction = true; //false;
-  
+    beta = true,
+    chisquare = true,
+    exponential = true,
+    normal = true,
+    studentt = true,
+    uniform = true,
+    linear = true,
+    stepFunction = true,
+      
   //Global variables
-  var buttons,
+    buttons,
     el,
     row,
     rowDistributions,
-    firebtn;
+    firebtn,
   
   //UTILITIES functions
   
   //Retrieve forms data as dictionary
-  var getDataObj = function () {
-    
-    var dataArray = $('form.form-inline').serializeArray(),
-      dataObj = {};
+    getDataObj = function () {
 
-    $(dataArray).each(function (i, field) {
-      dataObj[field.name] = field.value;
-    });
-    
-    return dataObj;
-    
-  };
+      var dataArray = $('form.form-inline').serializeArray(),
+        dataObj = {};
+
+      $(dataArray).each(function (i, field) {
+        dataObj[field.name] = field.value;
+      });
+
+      return dataObj;
+
+    },
   
   //CSS style for the library elements
-  var setCSS = function () {
+    setCSS = function () {
     
-    var node = document.createElement('style');
-    node.innerHTML = ".putMeHere {background:#ffcccc} .distr-button { background:#ff8080; border-color:#ff6666; } .distr-button:focus, .distr-button:hover { background:#ff6666; border-color:#ff6666; outline: none !important; box-shadow: none;} .fire-btn{background:#cc0000; border-color:#b30000;} .fire-btn:focus, .fire-btn:hover {background:#b30000; border-color:#b30000; outline: none !important; box-shadow: none;} .no-bottom-margin {margin-bottom: 0px;}}";
-    document.body.appendChild(node);
+      var node = document.createElement('style');
+      node.innerHTML = ".putMeHere {background:#ffcccc} .distr-button { background:#ff8080; border-color:#ff6666; } .distr-button:focus, .distr-button:hover { background:#ff6666; border-color:#ff6666; outline: none !important; box-shadow: none;} .fire-btn{background:#cc0000; border-color:#b30000;} .fire-btn:focus, .fire-btn:hover {background:#b30000; border-color:#b30000; outline: none !important; box-shadow: none;} .no-bottom-margin {margin-bottom: 0px;}}";
+      document.body.appendChild(node);
     
-  };
+    },
+      
+    buildDistrButton = function (text) {
+    
+      var btn;
+      btn = document.createElement('button');
+      btn.type = 'button';
+      btn.className = 'btn btn-primary ml-2 mb-2 distr-button';
+      btn.textContent = text;
+
+      return btn;
+    
+    },
+  
+    generateSamples = function (distribution) {
+    
+      var dataObj = getDataObj(),
+        samples = [],
+        i = 0;
+
+      switch (distribution) {
+      case 'normal':
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          samples.push(jStat.normal.sample(dataObj.mean, dataObj.stdev));
+        }
+        return samples;
+      case 'beta':
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          samples.push(jStat.beta.sample(dataObj.alpha, dataObj.beta));
+        }
+        return samples;
+      case 'chisquare':
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          samples.push(jStat.chisquare.sample(dataObj.dof));
+        }
+        return samples;
+      case 'exp':
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          samples.push(jStat.exponential.sample(dataObj.rate));
+        }
+        return samples;
+      case 'uni':
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          samples.push(jStat.uniform.sample(dataObj.a, dataObj.b));
+        }
+        return samples;
+      case 'studentT':
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          samples.push(jStat.studentt.sample(dataObj.dof));
+        }
+        return samples;
+      case 'linear':
+          //TODO
+      case 'step':
+          //TODO
+      default:
+        return [];
+      }
+    
+    },
+    
+    defaultFireFunction = function () {
+      
+      var dataObj = getDataObj(),
+        i = 0;
+
+      console.log(dataObj);
+      if (dataObj.buttonNum == buttons.length) {
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          buttons[Math.floor((Math.random() * 3))].click();
+        }
+      } else {
+        for (i = 0; i < dataObj.numClick; i = i + 1) {
+          buttons[dataObj.buttonNum].click();
+        }
+      }
+
+    },
+
+    fireFunction = function (distribution) {
+
+      var dataObj = getDataObj(),
+        samples = generateSamples(distribution),
+        init = 0,
+        i = 0;
+
+      samples.sort((a, b) => a - b);
+      //TODO
+      //PLOT samples
+
+      init = samples[0];
+      samples[0] = 0;
+      for (i = 1; i < samples.length; i++) {
+        samples[i] = samples[i] - init;
+      }
+
+      console.log(samples);
+
+      if (dataObj.buttonNum == buttons.length){
+          for (i = 0; i < samples.length; i++) {
+            setTimeout(function(){buttons[Math.floor((Math.random() * 3))].click();}, samples[i]);
+          }
+        } else {
+          for (i = 0; i < samples.length; i++) {
+            setTimeout(function(){buttons[dataObj.buttonNum].click();}, samples[i]);
+
+          }
+      }
+
+    },
+      
+    showNormal = function () {
+    
+      var form = "<form class='form-inline col-md-12 col-sm-6 mt-2'><h5 class='col-md-3 col-sm-6 no-bottom-margin'>Normal distribution</h5><div class='form-group'><label for='meanText'>Mean:&nbsp</label><input type='text' class='form-control mr-sm-2' id='meanText' name='mean'/></div><div class='form-group'><label for='stdevText'>Standard Deviation:&nbsp</label><input type='text' class='form-control mr-sm-2' id='stdevText' name='stdev'/></div></form>";
+
+      rowDistributions.innerHTML = form;
+
+      firebtn.onclick = function () { fireFunction('normal'); };
+
+      this.showChoose();
+    
+    },
+      
+    showBeta = function () {
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+      
+    showChiSquare = function () { 
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+      
+    showExp = function () { 
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+      
+    showUni = function () { 
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+      
+    showStudentT = function () { 
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+      
+    showLinear = function () { 
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+      
+    showStep = function () { 
+    rowDistributions.innerHTML = '';
+    this.showChoose(); },
+  
+    showChoose = function () { 
+    rowDistributions.innerHTML += "<button type='button' class='btn btn-secondary choose-btn ml-2 mb-2' onClick='" + varName + ".distributionButtonsHTML()'>Back</button>";
+    };
   
   //INITIALIZATION function
   this.build = function () {
@@ -119,154 +270,40 @@ function MakeRequests(opt) {
   
   this.distributionButtonsHTML = function () {
     
-    var builtHTML = '',
-      init = "<button type='button' class='btn btn-primary ml-2 mb-2 distr-button ",
-      end = "</button>";
+    var btn;
     
-    if (normal) { builtHTML += init + "normal-btn' onclick='" + varName + ".showNormal()'>Normal" + end; }
-    if (beta) { builtHTML += init + "beta-btn' onclick='makeRequests.showBeta()'>Beta" + end; }
-    if (chisquare) { builtHTML += init + "chisquare-btn' onclick='" + varName + ".showChiSquare()'>Chi Square" + end; }
-    if (exponential) { builtHTML += init + "exp-btn' onclick='" + varName + ".showExp()'>Exponential" + end; }
-    if (uniform) { builtHTML += init + "uni-btn' onclick='" + varName + ".showUni()'>Uniform" + end; }
-    if (studentt) { builtHTML += init + "studentt-btn' onclick='" + varName + ".showStudentT()'>T-Student" + end; }
-    if (linear) { builtHTML += init + "linear-btn' onclick='" + varName + ".showLinear()'>Linear" + end; }
-    if (stepFunction) { builtHTML += init + "step-btn' onclick='" + varName + ".showStep()'>Step function" + end; }
-    
-    rowDistributions.innerHTML = builtHTML;
-    
-  };
-  
-  this.showNormal = function () {
-    
-    var form = "<form class='form-inline col-md-12 col-sm-6 mt-2'><h5 class='col-md-3 col-sm-6 no-bottom-margin'>Normal distribution</h5><div class='form-group'><label for='meanText'>Mean:&nbsp</label><input type='text' class='form-control mr-sm-2' id='meanText' name='mean'/></div><div class='form-group'><label for='stdevText'>Standard Deviation:&nbsp</label><input type='text' class='form-control mr-sm-2' id='stdevText' name='stdev'/></div></form>";
-    
-    rowDistributions.innerHTML = form;
-    
-    firebtn.onclick = function() {fireFunction('normal', buttons)};
-    
-    this.showChoose();
-    
-  };
-  this.showBeta = function () {
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  this.showChiSquare = function () { 
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  this.showExp = function () { 
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  this.showUni = function () { 
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  this.showStudentT = function () { 
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  this.showLinear = function () { 
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  this.showStep = function () { 
-    rowDistributions.innerHTML = '';
-    this.showChoose(); };
-  
-  this.showChoose = function () { 
-    rowDistributions.innerHTML += "<button type='button' class='btn btn-secondary choose-btn ml-2 mb-2' onClick='" + varName + ".distributionButtonsHTML()'>Back</button>";
-  };
-  
-  var generateSamples = function (distribution, dataObj) {
-    
-    var samples = [],
-      i = 0;
-      
-    switch(distribution) {
-    case 'normal':
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-         samples.push(jStat.normal.sample(dataObj.mean, dataObj.stdev));
-        }
-        return samples;
-    case 'beta':
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-         samples.push(jStat.beta.sample(dataObj.alpha, dataObj.beta));
-        }
-        return samples;
-    case 'chisquare':
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-         samples.push(jStat.chisquare.sample(dataObj.dof));
-        }
-        return samples;
-    case 'exp':
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-         samples.push(jStat.exponential.sample(dataObj.rate));
-        }
-        return samples;
-    case 'uni':
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-         samples.push(jStat.uniform.sample(dataObj.a, dataObj.b));
-        }
-        return samples;
-    case 'studentT':
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-         samples.push(jStat.studentt.sample(dataObj.dof));
-        }
-        return samples;
-    case 'linear':
-        //TODO
-    case 'step':
-        //TODO
-    default:
-        return [];
-    }
-    
-  };
-  
-  var defaultFireFunction = function () {
-      
-      console.log("Fire");
-      var dataObj = getDataObj(),
-          i = 0;
-      
-      console.log(dataObj);
-      if (dataObj.buttonNum == buttons.length) {
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-          buttons[Math.floor((Math.random() * 3))].click();
-        }
-      } else {
-        for (i = 0; i < dataObj.numClick; i = i + 1) {
-          buttons[dataObj.buttonNum].click();
-        }
-      }
-      
-  };
-  
-  var fireFunction = function (distribution, buttons) {
-        
-    var dataObj = getDataObj(),
-        samples = generateSamples(distribution, dataObj),
-        init = 0,
-        i = 0;
-
-    samples.sort((a, b) => a - b);
-    //TODO
-    //PLOT samples
-
-    init = samples[0];
-    samples[0] = 0;
-    for (i = 1; i < samples.length; i++) {
-      samples[i] = samples[i] - init;
-    }
-
-    console.log(samples);
-
-    if (dataObj.buttonNum == buttons.length){
-        for (i = 0; i < samples.length; i++) {
-          setTimeout(function(){buttons[Math.floor((Math.random() * 3))].click();}, samples[i]);
-        }
-      } else {
-        for (i = 0; i < samples.length; i++) {
-          setTimeout(function(){buttons[dataObj.buttonNum].click();}, samples[i]);
-
-        }
-    }
+    if (normal) { 
+      btn = buildDistrButton('Normal');
+      btn.onclick = function() { showNormal(); };
+      rowDistributions.appendChild(btn) };
+    if (beta) { 
+      btn = buildDistrButton('Beta');
+      btn.onclick = function() { showBeta(); };
+      rowDistributions.appendChild(btn) };
+    if (chisquare) { 
+      btn = buildDistrButton('Chi Square');
+      btn.onclick = function() { showChiSquare(); };
+      rowDistributions.appendChild(btn) };
+    if (exponential) { 
+      btn = buildDistrButton('Exponential');
+      btn.onclick = function() { showExp(); };
+      rowDistributions.appendChild(btn) };
+    if (uniform) { 
+      btn = buildDistrButton('Uniform');
+      btn.onclick = function() { showUni(); };
+      rowDistributions.appendChild(btn) };
+    if (studentt) { 
+      btn = buildDistrButton('T-Student');
+      btn.onclick = function() { showStudentT(); };
+      rowDistributions.appendChild(btn) };
+    if (linear) { 
+      btn = buildDistrButton('Linear');
+      btn.onclick = function() { showLinear(); };
+      rowDistributions.appendChild(btn) };
+    if (stepFunction) { 
+      btn = buildDistrButton('Step Function');
+      btn.onclick = function() { showStep(); };
+      rowDistributions.appendChild(btn) };
     
   };
    
